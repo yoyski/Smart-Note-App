5a7c128a07cd3288b0930e52687f0a50ad4edc20
yoyski <148410739+yoyski@users.noreply.github.com>
2026-02-11 20:25:38 +0800

    remove suspense

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router";
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 
 const HomePage = lazy(() => import("./pages/HomePage.jsx"));
 const CreatePage = lazy(() => import("./pages/CreatePage.jsx"));
@@ -10,7 +10,6 @@ const ViewPage = lazy(() => import("./pages/ViewPage.jsx"));
 const App = () => {
   return (
     <div>
-      <Suspense fallback={<div>Loading page...</div>}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="create" element={<CreatePage />} />
@@ -18,7 +17,6 @@ const App = () => {
           <Route path="note/:id" element={<ViewPage />} />
           <Route path="note/:id/edit" element={<NoteEditPage />} />
         </Routes>
-      </Suspense>
     </div>
   );
 };
